9e9e7c7a411f2d5fc07b594c97cf963dc0fb6f20
藤田勇一 <221065@st.yoshida-g.ac.jp>
2024-01-30 23:35:35 +0900

    起伏エディター起動できない問題修正 デバッグ文字の色白に

--- 01_MagicalTop/02_エディター/起伏エディター/project000_起伏エディター/useful.h
+++ 01_MagicalTop/02_エディター/起伏エディター/project000_起伏エディター/useful.h
@@ -122,7 +122,7 @@ namespace useful
 		const T min,	// 最小範囲
 		const T max		// 最大範囲
 	);
-	template<class T> void useful::SortNum	// 値のソート
+	template<class T> void SortNum	// 値のソート
 	( // 引数
 		T *pSortNum,		// ソート配列
 		const int nMaxKeep	// 配列サイズ
